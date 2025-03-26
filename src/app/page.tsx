cf8c8d553b6f0b28779b956b1c95004902429b3f
Edgar Trejo Avila <edgar.avila.rro@gmail.com>
2025-03-26 08:12:34 -0600

    Added boilerplate as a proof of concept

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,9 @@ export default function Home() {
           height={38}
           priority
         />
+        <h1>
+          Sweet Goals
+        </h1>
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
