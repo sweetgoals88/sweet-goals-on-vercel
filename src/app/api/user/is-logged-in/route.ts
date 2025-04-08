b96380218af746819409f5272a90ec7e6e7a42df
Edgar Trejo Avila <edgar.avila.rro@gmail.com>
2025-04-08 08:43:41 -0600

    Removed invalid request type (again)

--- src/app/api/user/is-logged-in/route.ts
+++ src/app/api/user/is-logged-in/route.ts
@@ -1,9 +1,9 @@
-import { NextApiRequest } from "next";
 import { authenticateUser } from "../../lib/authenticate-user";
 import { cookies } from "next/headers";
 import { makeErrorResponse } from "../../lib/make-error-response";
+import { NextRequest } from "next/server";
 
-export async function POST(request: NextApiRequest) {
+export async function POST(request: NextRequest) {
   try {
     await authenticateUser(() => cookies(), {});
     console.log("Authentication passed");
